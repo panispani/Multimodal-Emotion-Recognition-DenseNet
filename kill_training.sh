d2cb220943e86dcb749942de7976aef2e6d8df48
panpan2 <p.panayiotou2@gmail.com>
2018-01-27 17:59:25 +0000

    Add script to kill training and eval processes - doesn't delete the model

---
+++ kill_training.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PID=`ps -ef | grep pp3414 | grep emotion_train | grep -v grep | awk '{print $2}'`
+kill -KILL $PID
+PID=`ps -ef | grep pp3414 | grep emotion_eval | grep -v grep | awk '{print $2}'`
+for i in $PID; do
+    kill -KILL $i
+done
