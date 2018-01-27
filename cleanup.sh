cf8dd0ed0563cfb268c0c9522c836a650d8bcb3b
panpan2 <p.panayiotou2@gmail.com>
2018-01-27 19:29:47 +0000

    Refactor cleanup scripts, add extra clause to delete all logs

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 rm -rfv ckpt/train/*
-rm -fv ckpt/log/*
+rm -fv ckpt/log/train/*
+rm -fv ckpt/log/valid/*
+rm -fv eval_train.out
+rm -fv eval_valid.out
+rm -fv "nohup.out"
+
+
--- newtrain.sh
+++ newtrain.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # cleanup
-rm -rfv ckpt/train/*
-rm -fv ckpt/log/*
-rm -fv eval_train.out
-rm -fv eval_valid.out
-rm -fv "nohup.out"
+./cleanup.sh
 
 # fire new background processes
 nohup python emotion_eval.py &> eval_train.out &
